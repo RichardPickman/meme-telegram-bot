ec6ac0ef55401fc298e37aca03d3d1cca1ca2363
RichardPickman <drezzerock@gmail.com>
2024-11-05 12:27:13 +0100

    add env vars mapper

--- lib/meme-telegram-bot-stack.ts
+++ lib/meme-telegram-bot-stack.ts
@@ -3,9 +3,30 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import path from 'path';
+import {
+    TELEGRAM_BOT_TOKEN,
+    TELEGRAM_MEME_CHANNEL_ID,
+    TELEGRAM_PROPOSAL_CHANNEL_ID,
+    TESTING_BOT_TOKEN,
+    TESTING_MEME_CHANNEL_ID,
+    TESTING_PROPOSAL_CHANNEL_ID,
+} from './environments';
 import { commonLambdaProps, rootDir } from './helpers';
 
 const lambdaPath = path.join(rootDir, 'services');
+
+const envVars = {
+    prod: {
+        TELEGRAM_BOT_TOKEN: TELEGRAM_BOT_TOKEN!,
+        TELEGRAM_PROPOSAL_CHANNEL_ID: TELEGRAM_PROPOSAL_CHANNEL_ID!,
+        TELEGRAM_MEME_CHANNEL_ID: TELEGRAM_MEME_CHANNEL_ID!,
+    },
+    test: {
+        TELEGRAM_BOT_TOKEN: TESTING_BOT_TOKEN!,
+        TELEGRAM_PROPOSAL_CHANNEL_ID: TESTING_PROPOSAL_CHANNEL_ID!,
+        TELEGRAM_MEME_CHANNEL_ID: TESTING_MEME_CHANNEL_ID!,
+    },
+};
 
 export class MemeTelegramBotStack extends Stack {
     constructor(
@@ -23,11 +44,7 @@ export class MemeTelegramBotStack extends Stack {
                 ...commonLambdaProps,
                 entry: path.join(lambdaPath, 'telegram-listener.ts'),
                 environment: {
-                    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN!,
-                    TELEGRAM_PROPOSAL_CHANNEL_ID:
-                        process.env.TELEGRAM_PROPOSAL_CHANNEL_ID!,
-                    TELEGRAM_MEME_CHANNEL_ID:
-                        process.env.TELEGRAM_MEME_CHANNEL_ID!,
+                    ...envVars[stageName],
                     stageName,
                 },
             },
