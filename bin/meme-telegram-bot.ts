e830dfa7922f03d8bd3ea99e12c14fa023bcece5
RichardPickman <drezzerock@gmail.com>
2024-11-06 13:21:29 +0100

    remove all CodePipeline stack related code, due to its high expence

--- bin/meme-telegram-bot.ts
+++ bin/meme-telegram-bot.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
-import { PipelineStack } from '../lib/pipelines';
+import { MemeTelegramBotStack } from '../lib/meme-telegram-bot-stack';
 
 const app = new cdk.App();
 
-// new MemeTelegramBotStack(app, 'MemeTelegramBotStack', 'test');
-new PipelineStack(app, 'PipelineStack');
+new MemeTelegramBotStack(app, 'MemeTelegramBotStack');
 
 app.synth();
