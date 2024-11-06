ed301d463a003a7ececcbf623f136d74296e75bd
RichardPickman <drezzerock@gmail.com>
2024-11-06 14:12:47 +0100

    update stack for automated deployment

--- bin/meme-telegram-bot.ts
+++ bin/meme-telegram-bot.ts
@@ -5,6 +5,7 @@ import { MemeTelegramBotStack } from '../lib/meme-telegram-bot-stack';
 
 const app = new cdk.App();
 
-new MemeTelegramBotStack(app, 'MemeTelegramBotStack');
+new MemeTelegramBotStack(app, 'MemeTelegramBotStack-testing');
+new MemeTelegramBotStack(app, 'MemeTelegramBotStack-production');
 
 app.synth();
--- lib/meme-telegram-bot-stack.ts
+++ lib/meme-telegram-bot-stack.ts
@@ -3,13 +3,44 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import path from 'path';
+import {
+    TELEGRAM_BOT_TOKEN,
+    TELEGRAM_MEME_CHANNEL_ID,
+    TELEGRAM_PROPOSAL_CHANNEL_ID,
+    TESTING_BOT_TOKEN,
+    TESTING_MEME_CHANNEL_ID,
+    TESTING_PROPOSAL_CHANNEL_ID,
+} from './environments';
 import { commonLambdaProps, rootDir } from './helpers';
 
 const lambdaPath = path.join(rootDir, 'services');
 
+const environmentVariables = {
+    production: {
+        TELEGRAM_BOT_TOKEN: TELEGRAM_BOT_TOKEN!,
+        TELEGRAM_PROPOSAL_CHANNEL_ID: TELEGRAM_PROPOSAL_CHANNEL_ID!,
+        TELEGRAM_MEME_CHANNEL_ID: TELEGRAM_MEME_CHANNEL_ID!,
+    },
+    testing: {
+        TELEGRAM_BOT_TOKEN: TESTING_BOT_TOKEN!,
+        TELEGRAM_PROPOSAL_CHANNEL_ID: TESTING_PROPOSAL_CHANNEL_ID!,
+        TELEGRAM_MEME_CHANNEL_ID: TESTING_MEME_CHANNEL_ID!,
+    },
+};
+
 export class MemeTelegramBotStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const stageName = id.split('-');
+
+        if (!stageName[1]) {
+            throw new Error(
+                'Stage name is not valid. Must be set as - stageName-testing or stageName-production',
+            );
+        }
+
+        const stage = stageName[1] as 'testing' | 'production';
 
         const memeTelegramBotHandler = new NodejsFunction(
             this,
@@ -18,11 +49,7 @@ export class MemeTelegramBotStack extends Stack {
                 ...commonLambdaProps,
                 entry: path.join(lambdaPath, 'telegram-listener.ts'),
                 environment: {
-                    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN!,
-                    TELEGRAM_PROPOSAL_CHANNEL_ID:
-                        process.env.TELEGRAM_PROPOSAL_CHANNEL_ID!,
-                    TELEGRAM_MEME_CHANNEL_ID:
-                        process.env.TELEGRAM_MEME_CHANNEL_ID!,
+                    ...environmentVariables[stage],
                 },
             },
         );
