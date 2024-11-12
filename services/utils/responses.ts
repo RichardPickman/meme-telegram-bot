9094bed123aebc5be77ae7e4f3133a2b35a8fcf1
RichardPickman <drezzerock@gmail.com>
2024-11-12 16:47:22 +0100

    remove excessive response due to broke behaviour

--- services/utils/responses.ts
+++ services/utils/responses.ts
@@ -1,13 +1,9 @@
 export const ErrorResponse = (message: string) => ({
     statusCode: 200,
-    status: 'error',
-    message: message,
 });
 
 export const SuccessfullResponse = (
     message: string = 'Success. Nothing to report.',
 ) => ({
     statusCode: 200,
-    status: 'success',
-    message: message,
 });
