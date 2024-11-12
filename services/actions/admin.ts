1968323a4da49b5145c1fac0bd5b9ca45bec2270
RichardPickman <drezzerock@gmail.com>
2024-11-12 12:45:47 +0100

    pick actions from mian file into own separate ones

---
+++ services/actions/admin.ts
@@ -0,0 +1,119 @@
+import { CallbackQuery } from 'node-telegram-bot-api';
+import {
+    TELEGRAM_MEME_CHANNEL_ID,
+    TELEGRAM_PROPOSAL_CHANNEL_ID,
+} from '../../lib/environments';
+import { bot } from '../instances/bot';
+import { isAdmin } from '../utils/booleans';
+import { ErrorResponse, SuccessfullResponse } from '../utils/responses';
+
+const cleanUpAfterAction = async (
+    memeId: number,
+    controlsId: number,
+    caption: 'Approved' | 'Declined',
+) => {
+    console.log('Cleaning up after action...');
+
+    try {
+        await bot.editMessageCaption(caption, {
+            chat_id: TELEGRAM_PROPOSAL_CHANNEL_ID!,
+            message_id: memeId,
+        });
+
+        await bot.deleteMessage(TELEGRAM_PROPOSAL_CHANNEL_ID!, controlsId);
+
+        return;
+    } catch (e) {
+        console.log('Error cleaning up after action. Error: ', e);
+
+        return ErrorResponse('Error cleaning up after action');
+    }
+};
+
+export const proceedWithAdminAction = async (
+    body: Body & { callback_query: CallbackQuery },
+) => {
+    console.log('Proceeding with admin action...');
+
+    const data = body.callback_query.data;
+
+    if (!data) {
+        console.log('No data provided');
+
+        return ErrorResponse('No data provided');
+    }
+
+    const [action, messageId] = data.split('-') as [
+        'approve' | 'decline',
+        string,
+    ];
+
+    const isAllowedToProceed = await isAdmin(
+        body.callback_query.from.id,
+        TELEGRAM_PROPOSAL_CHANNEL_ID!,
+    );
+
+    if (!isAllowedToProceed) {
+        console.log('User is not allowed to proceed');
+
+        if (!body.callback_query.message?.chat.id) {
+            console.log('No chat id provided');
+
+            return ErrorResponse('No chat id provided');
+        }
+
+        await bot.answerCallbackQuery(body.callback_query.id, {
+            text: 'You are not allowed to proceed 🖕🏻',
+        });
+
+        return ErrorResponse('User is not allowed to proceed');
+    }
+
+    if (action === 'approve') {
+        await bot.forwardMessage(
+            TELEGRAM_MEME_CHANNEL_ID!,
+            TELEGRAM_PROPOSAL_CHANNEL_ID!,
+            Number(messageId),
+        );
+
+        await bot.answerCallbackQuery(body.callback_query.id, {
+            text: 'Meme sent 🎉',
+        });
+
+        if (!body.callback_query.message?.message_id) {
+            console.log('No message id provided');
+
+            return ErrorResponse('No message id provided');
+        }
+
+        await cleanUpAfterAction(
+            Number(messageId),
+            body.callback_query.message.message_id,
+            'Approved',
+        );
+
+        return SuccessfullResponse();
+    }
+
+    if (!body.callback_query.message?.message_id) {
+        console.log('No message id provided');
+
+        return ErrorResponse('No message id provided');
+    }
+
+    if (action === 'decline') {
+        await bot.answerCallbackQuery(body.callback_query.id, {
+            text: 'Got you boss, I will not send this one 🫡',
+        });
+
+        await cleanUpAfterAction(
+            Number(messageId),
+            body.callback_query.message.message_id,
+            'Declined',
+        );
+
+        return SuccessfullResponse();
+    }
+
+    return SuccessfullResponse();
+};
---
+++ services/actions/proposal.ts
@@ -0,0 +1,90 @@
+import { Message, Update } from 'node-telegram-bot-api';
+import { TELEGRAM_PROPOSAL_CHANNEL_ID } from '../../lib/environments';
+import { sendPhotoToChannel, sendVideoToChannel } from '../senders';
+import {
+    isForwardedMessage,
+    isMessage,
+    isPhotoParameterExist,
+    isVideoParameterExist,
+} from '../utils/booleans';
+import { ErrorResponse, SuccessfullResponse } from '../utils/responses';
+
+const handleProposal = async (data: Message) => {
+    console.log(data);
+
+    if (isPhotoParameterExist(data)) {
+        const photo = data.photo?.at(-1);
+
+        if (!photo) {
+            console.log('No photo provided');
+
+            return ErrorResponse('No photo provided');
+        }
+
+        const photoId = photo.file_id;
+
+        await sendPhotoToChannel(
+            photoId,
+            TELEGRAM_PROPOSAL_CHANNEL_ID!,
+            data.caption,
+        );
+
+        return SuccessfullResponse();
+    }
+
+    if (isVideoParameterExist(data)) {
+        const video = data.video;
+
+        if (!video) {
+            console.log('No video provided');
+
+            return ErrorResponse('No video provided');
+        }
+
+        await sendVideoToChannel(
+            video.file_id,
+            TELEGRAM_PROPOSAL_CHANNEL_ID!,
+            data.caption,
+        );
+
+        return SuccessfullResponse();
+    }
+
+    console.log('Data format is invalid.');
+
+    return ErrorResponse('Data format is invalid.');
+};
+
+export const proceedWithMemeProposal = async (body: Update) => {
+    console.log('Proceeding with media...');
+
+    const isForwarded = isForwardedMessage(body);
+
+    if (isForwarded) {
+        const channelPost = body.channel_post;
+
+        if (!channelPost) {
+            console.log('No photo provided');
+
+            return ErrorResponse('No photo provided');
+        }
+
+        return await handleProposal(channelPost);
+    }
+
+    const isDirectMessage = isMessage(body);
+
+    if (isDirectMessage) {
+        const message = body.message;
+
+        if (!message) {
+            console.log('No photo provided');
+
+            return ErrorResponse('No photo provided');
+        }
+
+        return await handleProposal(message);
+    }
+
+    return SuccessfullResponse();
+};
