6ad44b8aefa5bdcb76916f347b0d503c2bbb3377
RichardPickman <drezzerock@gmail.com>
2024-11-12 12:45:27 +0100

    add env vars existance validators

--- lib/environments.ts
+++ lib/environments.ts
@@ -11,3 +11,17 @@ export const TESTING_BOT_TOKEN = process.env.TESTING_BOT_TOKEN;
 export const TESTING_PROPOSAL_CHANNEL_ID =
     process.env.TESTING_PROPOSAL_CHANNEL_ID;
 export const TESTING_MEME_CHANNEL_ID = process.env.TESTING_MEME_CHANNEL_ID;
+
+export const isTestingVariablesSet = () =>
+    [
+        TESTING_BOT_TOKEN,
+        TESTING_PROPOSAL_CHANNEL_ID,
+        TESTING_MEME_CHANNEL_ID,
+    ].every(Boolean);
+
+export const isProductionVariablesSet = () =>
+    [
+        TELEGRAM_BOT_TOKEN,
+        TELEGRAM_PROPOSAL_CHANNEL_ID,
+        TELEGRAM_MEME_CHANNEL_ID,
+    ].every(Boolean);
