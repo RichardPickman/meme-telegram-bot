3bdf367317c24dc9948a3ea180990b2f603c676b
RichardPickman <drezzerock@gmail.com>
2024-11-15 19:18:16 +0100

    add media data helper

--- services/utils/helpers.ts
+++ services/utils/helpers.ts
@@ -1,4 +1,36 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { Message } from 'node-telegram-bot-api';
+
+type MediaDataOutput = {
+    type: 'photo' | 'video';
+    media: string;
+};
 
 export const getBodyOrNull = (event: APIGatewayProxyEvent) =>
     event.body ? JSON.parse(event.body) : null;
+
+export const getMediaDataFromMessage = (
+    message: Message,
+): MediaDataOutput | null => {
+    if (message.photo) {
+        const photo = message.photo.at(-1);
+
+        if (!photo) {
+            return null;
+        }
+
+        return {
+            type: 'photo',
+            media: photo.file_id,
+        };
+    }
+
+    if (message.video) {
+        return {
+            type: 'video',
+            media: message.video?.file_id,
+        };
+    }
+
+    return null;
+};
