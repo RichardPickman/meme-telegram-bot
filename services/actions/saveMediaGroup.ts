17aa1e5f55358e55aacaa6838941252b63c2055d
RichardPickman <drezzerock@gmail.com>
2024-11-15 14:21:43 +0100

    add media group handler

---
+++ services/actions/saveMediaGroup.ts
@@ -0,0 +1,54 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+    DynamoDBDocumentClient,
+    GetCommand,
+    PutCommand,
+} from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const MEDIA_TABLE_NAME = process.env.MEDIA_TABLE_NAME || '';
+
+export const saveGroupData = async (mediaId: string, messageId: number) => {
+    console.log('Saving media group data...');
+
+    const putCommand = new PutCommand({
+        TableName: MEDIA_TABLE_NAME,
+        Item: {
+            mediaId,
+            messageId,
+        },
+    });
+
+    try {
+        const response = await docClient.send(putCommand);
+
+        return response;
+    } catch (e) {
+        console.log('Error saving media group data: ', e);
+
+        return null;
+    }
+};
+
+export const getMessageId = async (mediaId: string) => {
+    console.log('Getting message id...');
+
+    const getCommand = new GetCommand({
+        TableName: MEDIA_TABLE_NAME,
+        Key: {
+            mediaId,
+        },
+    });
+
+    try {
+        const response = await docClient.send(getCommand);
+
+        return response.Item?.messageId;
+    } catch (e) {
+        console.log('Error getting message id: ', e);
+
+        return null;
+    }
+};
