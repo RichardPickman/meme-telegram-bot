726a542bf53bfa7fa0c18ad2cb76d5c676a73e7d
RichardPickman <drezzerock@gmail.com>
2024-11-26 16:43:14 +0100

    add proposal db for delayed posting

--- lib/meme-telegram-bot-stack.ts
+++ lib/meme-telegram-bot-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import path from 'path';
@@ -10,6 +11,17 @@ const lambdaPath = path.join(rootDir, 'src');
 export class MemeTelegramBotStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const proposalDb = new Table(this, 'MyTable', {
+            partitionKey: { name: 'id', type: AttributeType.STRING },
+            sortKey: { name: 'createdAt', type: AttributeType.NUMBER },
+        });
+
+        proposalDb.addGlobalSecondaryIndex({
+            indexName: 'CreatedAtIndex',
+            partitionKey: { name: 'id', type: AttributeType.STRING },
+            sortKey: { name: 'createdAt', type: AttributeType.NUMBER },
+        });
 
         const memeTelegramBotHandler = new NodejsFunction(
             this,
@@ -23,6 +35,7 @@ export class MemeTelegramBotStack extends Stack {
                         process.env.TELEGRAM_PROPOSAL_CHANNEL_ID!,
                     TELEGRAM_MEME_CHANNEL_ID:
                         process.env.TELEGRAM_MEME_CHANNEL_ID!,
+                    MEME_DATABASE_TABLE_NAME: proposalDb.tableName,
                 },
             },
         );
