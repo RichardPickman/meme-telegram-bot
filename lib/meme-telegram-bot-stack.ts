64516505670aacb670b6bb8b6379f29bf6d857c2
RichardPickman <drezzerock@gmail.com>
2024-11-26 16:57:33 +0100

    add forgotten handler and rule into stack definition

--- lib/meme-telegram-bot-stack.ts
+++ lib/meme-telegram-bot-stack.ts
@@ -1,6 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import path from 'path';
@@ -39,6 +41,27 @@ export class MemeTelegramBotStack extends Stack {
                 },
             },
         );
+
+        const memePublisherHandler = new NodejsFunction(
+            this,
+            'Meme Telegram Bot',
+            {
+                ...commonLambdaProps,
+                entry: path.join(lambdaPath, 'publishMeme.ts'),
+                environment: {
+                    TELEGRAM_PROPOSAL_CHANNEL_ID:
+                        process.env.TELEGRAM_PROPOSAL_CHANNEL_ID!,
+                    TELEGRAM_MEME_CHANNEL_ID:
+                        process.env.TELEGRAM_MEME_CHANNEL_ID!,
+                },
+            },
+        );
+
+        const rule = new Rule(this, 'Rule', {
+            schedule: Schedule.cron({ minute: '*/2' }),
+        });
+
+        rule.addTarget(new LambdaFunction(memePublisherHandler));
 
         const api = new RestApi(this, 'MemeTelegramBot', {
             restApiName: 'MemeTelegramBot',
