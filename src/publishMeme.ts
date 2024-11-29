f03e7bb2858ce10ade78f9190942f161f25265ed
RichardPickman <drezzerock@gmail.com>
2024-11-29 14:07:48 +0100

    add env vars checker to publishMeme handler

--- src/publishMeme.ts
+++ src/publishMeme.ts
@@ -4,10 +4,21 @@ import { getCurrentTimeFrameMeme, updateMeme } from './utils/database';
 
 const TELEGRAM_PROPOSAL_CHANNEL_ID = process.env.TELEGRAM_PROPOSAL_CHANNEL_ID;
 const TELEGRAM_MEME_CHANNEL_ID = process.env.TELEGRAM_MEME_CHANNEL_ID;
+const MEME_DATABASE_TABLE_NAME = process.env.MEME_DATABASE_TABLE_NAME;
+
+const ENV_VARS = [
+    TELEGRAM_PROPOSAL_CHANNEL_ID,
+    TELEGRAM_MEME_CHANNEL_ID,
+    MEME_DATABASE_TABLE_NAME,
+];
+
+if (!ENV_VARS.every((envVar) => envVar)) {
+    throw new Error('One or more env vars are missing');
+}
 
 export const handler = async () => {
     const meme = (await getCurrentTimeFrameMeme(
-        process.env.MEME_DATABASE_TABLE_NAME!,
+        MEME_DATABASE_TABLE_NAME!,
     )) as Meme | null;
 
     if (!meme) {
