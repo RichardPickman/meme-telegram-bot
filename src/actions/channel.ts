54893a11cb391f016acb76d9b162481ccacac75c
RichardPickman <drezzerock@gmail.com>
2024-12-01 09:48:44 +0100

    add channel action

--- src/actions/channel.ts
+++ src/actions/channel.ts
@@ -1,5 +1,25 @@
-export const proceedWithChannelAction = (data: any) => {
-    // comment for rebuild
+import { Update } from 'node-telegram-bot-api';
+import { setReactionToPost } from '../senders';
+import { ErrorResponse } from '../utils/responses';
 
-    console.log(data);
+export const proceedWithChannelAction = async (data: Update) => {
+    console.log('Proceeding with channel action...');
+
+    const message = data.message;
+
+    if (!message) {
+        console.log('No message provided');
+
+        return ErrorResponse('No message provided');
+    }
+
+    const chatId = message.chat.id;
+
+    if (!chatId) {
+        console.log('No chat id provided');
+
+        return ErrorResponse('No chat id provided');
+    }
+
+    return await setReactionToPost(message.message_id, chatId);
 };
