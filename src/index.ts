122824a0b488ca8ff7c39eccbe6f029391023ea9
RichardPickman <drezzerock@gmail.com>
2024-12-04 15:03:19 +0100

    switch from APIGatewayProxyEvent to SQS Event and proceed in loop for each entry

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
+import { SQSEvent } from 'aws-lambda';
 import { proceedWithAdminAction } from './actions/admin';
 import { proceedWithChannelAction } from './actions/channel';
 import { proceedWithMemeProposal } from './actions/proposal';
@@ -25,42 +25,50 @@ if (!ENV_VARS.every(Boolean)) {
     throw new Error('One or more environmental variables are not set');
 }
 
-export const handler = async (event: APIGatewayProxyEvent) => {
+export const handler = async (event: SQSEvent) => {
     console.log('Starting handler...');
 
-    const body = getBodyOrNull(event);
+    for (const record of event.Records) {
+        const body = getBodyOrNull(record);
 
-    if (!body) {
-        console.log('Invalid event');
+        if (!body) {
+            console.log('Invalid event');
 
-        return ErrorResponse('Invalid event');
+            return ErrorResponse('Invalid event');
+        }
+
+        console.log('Proceed with body: ', body);
+
+        // Check chat type presence
+        const isMemeProposal = isMessageContainPrivateChatType(body.message);
+        const isSuitableAction = isActionContainChannelPostOrMessage(body);
+
+        if (isMemeProposal && isSuitableAction) {
+            console.log(
+                'Request is determined as meme proposal... Proceeding with media...',
+            );
+
+            return await proceedWithMemeProposal(body);
+        }
+
+        const isAdminAction = isMessageIsCallbackQuery(body);
+
+        if (isAdminAction) {
+            return await proceedWithAdminAction(body);
+        }
+
+        const isPostInChannel = isGroupPost(body.channel_post);
+
+        if (isPostInChannel) {
+            return await proceedWithChannelAction(body);
+        }
+
+        return SuccessfullResponse('Handler ended without errors');
     }
 
-    console.log('Proceed with body: ', body);
+    // If no record is found
 
-    // Check chat type presence
-    const isMemeProposal = isMessageContainPrivateChatType(body.message);
-    const isSuitableAction = isActionContainChannelPostOrMessage(body);
-
-    if (isMemeProposal && isSuitableAction) {
-        console.log(
-            'Request is determined as meme proposal... Proceeding with media...',
-        );
-
-        return await proceedWithMemeProposal(body);
-    }
-
-    const isAdminAction = isMessageIsCallbackQuery(body);
-
-    if (isAdminAction) {
-        return await proceedWithAdminAction(body);
-    }
-
-    const isPostInChannel = isGroupPost(body.channel_post);
-
-    if (isPostInChannel) {
-        return await proceedWithChannelAction(body);
-    }
+    console.log('No record found');
 
     return SuccessfullResponse('Handler ended without errors');
 };
